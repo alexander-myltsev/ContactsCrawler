7c326a27e37d85cbfec19e7c720dc5a09dbe498c
Alexander Myltsev <alexander.myltsev@phystech.edu>
2012-02-20 21:46:01 +0400

    Authors data fetching from MS academical search is done.

---
+++ src/AcademicData.scala
@@ -0,0 +1,85 @@
+import java.io.FileWriter
+import java.lang.Exception
+import java.net.URI
+import java.text.SimpleDateFormat
+import java.util.Date
+import xml.XML
+
+object AcademicData {
+  def fetchAuthor(id: Long) = {
+    // NOTE: An author Rss at academic.research.microsoft.com doesn't contain separate fields for organization,
+    // interests and homepage. All these fields are in CDATA. Additional RegEx parsing is required.
+
+    val requestUrl = new URI("http", "academic.research.microsoft.com", "/Rss/" + id, null).toURL
+    val conn = requestUrl.openConnection
+    val xml = XML.load(conn.getInputStream)
+
+    val Organization = """(?s)<p>([\w\d\s]+?)<br/></p>""".r
+    val InterestsAndHomepage = """(?s)<a href="http\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}/\S*?">(?!http://academic\.research\.microsoft\.com)(.+?)</a>""".r
+
+    val authorDescription = (xml \\ "item")(0) \\ "description"
+    val organization = (Organization findFirstIn authorDescription.text) match {
+      case Some(Organization(x)) => Some(x)
+      case None => None
+    }
+
+    def transform(it: Iterator[String], interests: Seq[String], buffer: String): (Seq[String], Option[String]) = {
+      // Returns: list of interests and homepage if the author has it.
+      if (it.hasNext) {
+        val InterestsAndHomepage(interestOrHomepage) = it.next
+        if (buffer.equals("")) transform(it, interests, interestOrHomepage)
+        else transform(it, interests :+ buffer, interestOrHomepage)
+      } else {
+        if (buffer.startsWith("""http://""")) (interests, Some(buffer))
+        else (interests :+ buffer, None)
+      }
+    }
+
+    val (interests, homepage) = transform(InterestsAndHomepage findAllIn authorDescription.text, Seq.empty[String], "")
+
+    println(organization)
+    println(interests.mkString("[Array:", " | ", "]"))
+    println(homepage)
+
+    val pp = new scala.xml.PrettyPrinter(80, 4)
+    val builder = new StringBuilder()
+    pp.format(xml, builder)
+    val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss")
+    val date = new Date
+    val fileWriter = new FileWriter("test-%s.xml".format(dateFormat.format(date)))
+    fileWriter.write(builder.toString)
+    fileWriter.close()
+
+    println(((xml \\ "item")(0) \\ "description").text)
+
+    //XML.save("test.xml", xml, "UTF8", true)
+  }
+
+  def getData(query: String) = {
+    val requestUrl = new URI("http", "academic.research.microsoft.com", "/Rss", "query=" + query + "&searchtype=0", null).toURL
+    val conn = requestUrl.openConnection
+    val xml = XML.load(conn.getInputStream)
+    val publications = xml \\ "item"
+    val AuthorRegex = """(?s)<a href="(http://academic\.research\.microsoft\.com/Author/)(\d+?)">(.+?)</a>""".r
+    val authorsIds: Set[Long] =
+      publications.foldLeft(Set.empty[Long])(
+        (ids, publication) => {
+          val description = publication \\ "description"
+          val publicationAuthors = AuthorRegex findAllIn description.text
+          val authorsIds = publicationAuthors.map(_ match {
+            case AuthorRegex(url, id, fullname) =>
+              //println(url + id + ": " + fullname)
+              id.toLong
+            case _ => throw new Exception("No author of publication?! " + publication.text)
+          })
+          ids ++ authorsIds
+        }
+      )
+    println(authorsIds)
+    //fetchAuthor(20757032)
+    //fetchAuthor(12737437)
+    //fetchAuthor(55621649)
+    //println(publications.length)
+  }
+
+}
--- src/Main.scala
+++ src/Main.scala
@@ -7,13 +7,21 @@ object Main {
     lines.toSet
   }
 
-  def main(args: Array[String]) = {
-    println(countries)
+  def bingDataTest() = {
     //val place = "Moscow State University"
     val place = "Maaaakro Royal Swedish Academy of Sciences"
     Bing.getData(place) match {
       case Some(x) => println(x.countryRegion + " | " + countries.contains(x.countryRegion))
       case None => println("NO DATA")
     }
+  }
+
+  def academDataTest() = {
+    AcademicData.getData("gromacs year>=2007")
+  }
+
+  def main(args: Array[String]) = {
+    //println(countries)
+    academDataTest()
   }
 }
