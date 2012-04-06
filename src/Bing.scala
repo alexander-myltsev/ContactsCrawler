14f95d5b4f490b8a9aee80dcc0007ea22d264f67
Alexander Myltsev <alexander.myltsev@gmail.com>
2012-04-06 14:56:39 +0400

    Bing downloader is more robust now.

--- src/Bing.scala
+++ src/Bing.scala
@@ -4,17 +4,30 @@ import io.Source
 import java.io.FileWriter
 import java.net.URI
 import java.text.SimpleDateFormat
-import java.util.Date
 import scala.collection.mutable.StringBuilder
-import xml.{Node, XML}
+import java.util.Date
 
-case class BingResponse(statusCode: Int, statusDescription: String, adminDistrict: String, countryRegion: String, /*locality: String,*/ isEME: Boolean) {
+object BingResponseStatus extends Enumeration {
+  type typ = Value
+
+  case class XmlParseFail(msg: String) extends Val(msg)
+
+  val XmlDownloadFail, SuccessSeveral, Success = Value
+}
+
+object BingResponse {
+  def apply(respStatus: BingResponseStatus.typ): BingResponse = BingResponse(respStatus, -1, null, null, null, false) // TODO: move it to case class BingResponse
+}
+
+case class BingResponse(respStatus: BingResponseStatus.typ, statusCode: Int, statusDescription: String,
+                        adminDistrict: String, countryRegion: String, /*locality: String,*/ isEME: Boolean) {
   override def toString =
-    "[BingResponse: statusCode = %d, statusDescription = %s, adminDistrict = %s, countryRegion = %s]".format(
-      statusCode, statusDescription, adminDistrict, countryRegion)
+    "[BingResponse: respStatus = %s, statusCode = %d, statusDescription = %s, adminDistrict = %s, countryRegion = %s]".format(
+      respStatus.toString, statusCode, statusDescription, adminDistrict, countryRegion)
 }
 
 object Bing {
+  private val bingKey = """AkwBTtVf8sI-lfyIFfZ-7FSQoxKT_qwkM9xPmCrEq73FUrEOQB2tK-4RUNknDPUj"""
   val countries = {
     val lines = for (line <- Source.fromFile("countries.txt").getLines)
     yield (line.split(" - ")(1))
@@ -24,7 +37,7 @@ object Bing {
   def dump(place: String, msg: String, url: String, node: xml.Node) = {
     //XML.save("dump.xml", node, "UTF8", true)
     val pp = new xml.PrettyPrinter(80, 4)
-    val builder = new StringBuilder()
+    val builder = new StringBuilder
     pp.format(node, builder)
     val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss-SSS")
     val date = new Date
@@ -33,12 +46,13 @@ object Bing {
     fileWriter.write("Place: " + place + "\n")
     fileWriter.write("URL: " + url + "\n\n")
     fileWriter.write(builder.toString)
-    fileWriter.close()
+    fileWriter.close
   }
 
-  def responseFromXML(node: xml.Node, countries: Set[String]) = {
+  def responseFromXML(node: xml.Node, countries: Set[String]): BingResponse = {
     if ((node \\ "ResourceSet").length > 1) {
-      throw new Exception("ResourceSets.length > 1")
+      //throw new Exception("ResourceSets.length > 1")
+      return BingResponse(BingResponseStatus.XmlParseFail("ResourceSets.length > 1"))
     }
     val resourceSet = node \\ "ResourceSet"
     val statusCode = (node \\ "StatusCode").text.toInt
@@ -46,27 +60,29 @@ object Bing {
     val estimatedTotal = (resourceSet \\ "EstimatedTotal").text.toInt
 
     if (estimatedTotal == 0) {
-      throw new Exception("EstimatedTotal == 0")
+      //throw new Exception("EstimatedTotal == 0")
+      BingResponse(BingResponseStatus.XmlParseFail("EstimatedTotal == 0"))
     } else if (estimatedTotal == 1) {
       val country = (resourceSet \\ "CountryRegion").text
-      new BingResponse(statusCode, statusDescription, (resourceSet \\ "AdminDistrict").text,
+      new BingResponse(BingResponseStatus.Success, statusCode, statusDescription, (resourceSet \\ "AdminDistrict").text,
         country, countries.contains(country))
     } else if (estimatedTotal > 1) {
       val locations = resourceSet \\ "Location"
       val locOpt = locations.find(l => countries.contains((l \\ "CountryRegion").text))
       locOpt match {
-        case None => throw new Exception("Countries don't contain location")
+        case None =>
+          //throw new Exception("Countries don't contain location")
+          BingResponse(BingResponseStatus.XmlParseFail("Countries don't contain location"))
         case Some(x) =>
-          new BingResponse(statusCode, statusDescription, (x \\ "AdminDistrict").text,
-            (x \\ "CountryRegion").text, true)
+          BingResponse(BingResponseStatus.SuccessSeveral, statusCode, statusDescription, (x \\ "AdminDistrict").text, (x \\ "CountryRegion").text, true)
       }
     } else {
-      throw new Exception("Unexpected EstimatedTotal: " + estimatedTotal)
+      //throw new Exception("Unexpected EstimatedTotal: " + estimatedTotal)
+      BingResponse(BingResponseStatus.XmlParseFail("Unexpected EstimatedTotal: " + estimatedTotal))
     }
   }
 
-  def getData(place: String) = {
-    val bingKey = """AkwBTtVf8sI-lfyIFfZ-7FSQoxKT_qwkM9xPmCrEq73FUrEOQB2tK-4RUNknDPUj"""
+  def getData(place: String): BingResponse = {
     // NOTE: http://msdn.microsoft.com/en-us/library/ff701714.aspx - BingMap REST API
     val xml =
       try {
@@ -76,17 +92,14 @@ object Bing {
           ex.printStackTrace()
           null
       }
-    if (xml == null) None
-    else
-      try {
-        val response = Bing.responseFromXML(xml, countries)
-        Some(response)
-      } catch {
-        case ex: Exception =>
-          ex.printStackTrace()
-          val url = new URI("http", "dev.virtualearth.net", "/REST/v1/Locations/-/-/-/" + place, "o=xml&key=" + bingKey, null).toURL
-          dump(place, ex.getMessage, url.toString, xml)
-          None
-      }
+    val response = {
+      if (xml == null) BingResponse(BingResponseStatus.XmlDownloadFail)
+      else Bing.responseFromXML(xml, countries)
+    }
+    if (response.respStatus != BingResponseStatus.Success) {
+      val url = new URI("http", "dev.virtualearth.net", "/REST/v1/Locations/-/-/-/" + place, "o=xml&key=" + bingKey, null).toURL
+      dump(place, response.respStatus.toString, url.toString, xml)
+    }
+    response
   }
 }
